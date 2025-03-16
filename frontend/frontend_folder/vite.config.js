8361753610ad7749ab78d5b31fde02d4a5428000
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-03-16 14:27:54 +0800

    update vite.config

--- frontend/frontend_folder/vite.config.js
+++ frontend/frontend_folder/vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "build", // <--- This sets the output folder to 'build' instead of 'dist'
+  },
 });
